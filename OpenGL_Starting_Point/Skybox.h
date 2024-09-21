0a114cf5805c9f2609cc6fbf1077f563263234e9
JT <godman0912@gmail.com>
2024-09-21 09:52:08 -0500

    Added Skybox class from bullet_physics_integration

---
+++ OpenGL_Starting_Point/Skybox.h
@@ -0,0 +1,34 @@
+// Skybox.h
+#pragma once
+
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <vector>
+#include <string>
+#include <iostream>
+
+class Skybox {
+public:
+    Skybox(const std::vector<std::string>& faces);
+    ~Skybox();
+
+    void draw(const glm::mat4& view, const glm::mat4& projection);
+
+    // Debug methods
+    void printDebugInfo() const;
+    bool isValid() const { return m_isValid; }
+
+private:
+    unsigned int loadCubemap(const std::vector<std::string>& faces);
+    void setupMesh();
+    void checkGLError(const char* operation);
+
+    unsigned int VAO, VBO;
+    unsigned int cubemapTexture;
+    unsigned int shaderProgram;
+    bool m_isValid;
+
+    // Debug info
+    int m_textureWidth, m_textureHeight;
+    std::string m_lastError;
+};
